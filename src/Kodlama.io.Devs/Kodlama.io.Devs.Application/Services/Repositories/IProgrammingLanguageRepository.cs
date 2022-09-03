61a770944e2f2e7bd57beb00800ad6b71806041b
Emre Akıncı <emreakinci696@gmail.com>
2022-09-03 14:43:59 +0300

    Add project files.

---
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.Application/Services/Repositories/IProgrammingLanguageRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kodlama.io.Devs.Domain.Entities;
+
+namespace Kodlama.io.Devs.Application.Services.Repositories
+{
+    public interface IProgrammingLanguageRepository: IAsyncRepository<ProgrammingLanguage>, IRepository<ProgrammingLanguage>
+    {
+    }
+}
